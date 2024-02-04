d175abc13550de1a0bfce8e2b27f0946efafd376
Ridwan <ridhwan.alabi@gmail.com>
2024-02-04 07:40:22 +0100

    Add useCartContext function

--- src/context/CartProvider.tsx
+++ src/context/CartProvider.tsx
@@ -1,3 +1,5 @@
+import { useMemo, useReducer } from "react"
+
 export type CartItemType = {
   sku: string,
   name: string,
@@ -66,7 +68,7 @@ const reducer = (state: CartStateType, action: ReducerAction): CartStateType => 
       }
 
       const updatedItem: CartItemType = { ...itemExists, qty }
-      
+
       return {...state, cart: [...filteredCart, updatedItem]}
     }
     case REDUCER_ACTON_TYPE.SUBMIT: {
@@ -75,4 +77,31 @@ const reducer = (state: CartStateType, action: ReducerAction): CartStateType => 
     default:
       throw new Error('Undefined reducer action type')
   }
+}
+
+
+const useCartContext = (initCartState: CartStateType) => {
+  const [state, dispatch] = useReducer(reducer, initCartState)
+
+  const REDUCER_ACTIONS = useMemo(() => {
+    return REDUCER_ACTON_TYPE
+  }, [])
+
+  const totalItems: number = state.cart.reduce((previousValue, cartItem) => {
+    return previousValue + cartItem.qty;
+  }, 0)
+
+  const totalPrice = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'NGN' }).format(
+    state.cart.reduce((previousValue, cartItem) => {
+      return previousValue + (cartItem.qty * cartItem.price)
+    }, 0)
+  )
+
+  const cart = state.cart.sort((a, b) => {
+    const itemA = Number(a.sku.slice(-4))
+    const itemB = Number(b.sku.slice(-4))
+    return itemA - itemB
+  })
+
+  return {dispatch, REDUCER_ACTIONS, totalItems, totalPrice, cart}
 }
