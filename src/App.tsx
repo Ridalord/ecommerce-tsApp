344dae6dd16f4e31b5dd3d943bd0448e52699bd1
Ridwan <ridhwan.alabi@gmail.com>
2024-02-05 11:29:27 +0100

    Add product list template

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react"
 import Header from "./components/Header"
+import Cart from "./components/Cart"
+import ProductList from "./components/ProductList"
+import Footer from "./components/Footer"
 
 
 function App() {
   const [viewCart, setViewCart] = useState(false)
 
+  const pageContent = viewCart ? <Cart /> : <ProductList />;
 
   const content = (
     <>
       <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart}/>
     </>
   )
 
