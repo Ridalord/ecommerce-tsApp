60e866b80b867a9095850e658a848efc5c0a4312
Ridwan <ridhwan.alabi@gmail.com>
2024-02-05 11:47:15 +0100

    Update productlist

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,4 +1,22 @@
+import { ReactElement } from "react";
+import useCart from "../hooks/useCart";
+import useProducts from "../hooks/useProducts";
+
 const ProductList = () => {
+  const { dispatch, REDUCER_ACTIONS, cart } = useCart()
+  const {products} = useProducts()
+
+  let pageContent: ReactElement | ReactElement[] = <p>Loading...</p>
+
+  if (products?.length) {
+    pageContent = products.map(product => {
+      const inCart: boolean = cart.some(item => item.sku === product.sku)
+
+      return (
+        
+      )
+    })
+  }
   return ( 
     <div>Product List</div>
    );
